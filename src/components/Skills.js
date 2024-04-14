999650e77735092b8c5a5ac112a39a0ebf853f04
ramisha-anwer <ramishaaa27@gmail.com>
2024-04-14 22:35:56 +0500

    skill updated

--- src/components/Skills.js
+++ src/components/Skills.js
@@ -1,20 +1,17 @@
 import Image from 'next/image'
 import React from 'react'
-import HTML from "../../public/images/skills/html.png"
-import CSS from "../../public/images/skills/css.png"
-import Node from "../../public/images/skills/node.png"
-import REACT from "../../public/images/skills/react.png"
-import JS from "../../public/images/skills/javascript.png"
 import AWS from "../../public/images/skills/aws.png"
-import AWSDARK from "../../public/images/skills/aws2.png"
-import TAILWIND from "../../public/images/skills/tailwind.png"
-import firebase from "../../public/images/skills/firebase.png"
-import github from "../../public/images/skills/github.png"
+import CSS from "../../public/images/skills/css.png"
 import githubDark from "../../public/images/skills/github3.png"
+import HTML from "../../public/images/skills/html.png"
+import JS from "../../public/images/skills/javascript.png"
 import mongo from "../../public/images/skills/mongo.png"
-import postgres from "../../public/images/skills/postgres.png"
 import sql from "../../public/images/skills/mysql.png"
-import express from "../../public/images/skills/express.png"
+import nextjs from "../../public/images/skills/nextjs.png"
+import Node from "../../public/images/skills/node.png"
+import postgres from "../../public/images/skills/postgres.png"
+import REACT from "../../public/images/skills/react.png"
+import TAILWIND from "../../public/images/skills/tailwind.png"
 import useThemeSwitcher from './hooks/useThemeSwitcher'
 
 function Skills() {
@@ -48,8 +45,8 @@ function Skills() {
               <p className='mt-4'>Mongo DB</p>
           </div>
           <div className='flex flex-col items-center justify-center shadow-lg shadow-gray-200 dark:shadow-gray-700 hover:scale-110 duration-500 py-4 rounded-md border-2 border-solid border-black dark:border-light'>
-              <Image className='w-[4rem] ' src={express}/>
-              <p className='mt-4'>Express js</p>
+              <Image className='w-[4rem] ' src={nextjs}/>
+              <p className='mt-4'>Next js</p>
           </div>
           <div className='flex flex-col items-center justify-center shadow-lg shadow-gray-200 dark:shadow-gray-700 hover:scale-110 duration-500 py-4 rounded-md border-2 border-solid border-black dark:border-light'>
               <Image className='w-[4rem]' src={TAILWIND}/>
